f9ead59ad59e98d3acae416796e8a35e2a9abbd8
Tomas Holas <tomas.holas@gmail.com>
2016-06-10 12:31:16 +0200

    react-bootstrap, observable cnt

--- src/app/components/root.jsx
+++ src/app/components/root.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {observer} from 'mobx-react';
+import Button from 'react-bootstrap/lib/Button';
+import autobind from 'autobind-decorator';
 
 @observer
 class Root extends React.Component {
 
+	@autobind
+	clickHandler(e) {
+		this.props.fooStore.updateCnt();
+	}
 	render() {
+		const {fooStore} = this.props;
 		return <div>
 			<h1>This is foo Root!</h1>
+			<h2>{fooStore.bar}</h2>
+			<h2>{fooStore.cnt}</h2>
+			<Button bsStyle="danger" onClick={this.clickHandler}>Danger</Button>
 		</div>
 	}
 }
--- src/app/stores/foo-store.js
+++ src/app/stores/foo-store.js
@@ -1,5 +1,19 @@
-import {observable} from "mobx";
+import {observable, action} from "mobx";
 
 export class FooStore {
 	@observable bar;
+	@observable cnt;
+
+	constructor() {
+		this.bar = "blabla";
+		this.cnt = 0;
+	}
+
+	@action	updateBar(text) {
+		this.bar = text;
+	}
+	@action	updateCnt() {
+		this.cnt++;
+	}
+
 }
